95e53da1f03ca368bc88b05e2c16a540f3e6fad2
Lucas Ruchel <lukasruchel@gmail.com>
2018-04-04 23:13:54 -0300

    Inicio da aula

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+#convert urls to dashed
+from django.utils.text import slugify
+
 # Create your models here.
+
+import misaka
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django import template
+register = template.Library()
+
+class Group(models.Model):
+    pass
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group,related_name='memberships',)
